2bad1bd0ba66918113b75e591b7be1518f6f451e
161250088 <161250088@smail.nju.edu.cn>
2020-04-28 00:15:02 +0800

    update Left.js

--- src/js/Left.js
+++ src/js/Left.js
@@ -1,34 +1,77 @@
 import React from 'react'
+import  userLogo from '../assets/circle.png'
 import checkLogo from '../assets/check.png'
 import manageLogo from '../assets/manage.png'
 import unlockLogo from '../assets/unlock.png'
 import avatar from '../assets/admin-avatar.png'
-import '../css/nav.css'
+import '../css/headSide.css'
+
+let firstStyle = {marginTop:'5%',backgroundColor: '#F2C94C'};
+let chosenStyle = {backgroundColor:'#E5E5E5'};
+let chosenTextStyle = {fontWeight: 'bold'};
+let otherStyle = {backgroundColor:'white'};
+let otherTextStyle = {fontWeight: 'normal'};
 
 function Left() {
     return (
-        <div className="left-nav">
-            <img src={avatar} className="avatar" alt="admin-avatar" />
-            <div className="user">
-                {/*<img src={userLogo} className="user-icon"  alt="user-icon" />*/}
-                <label className="username">admin</label></div>
-            <ul className="left">
-                <li>
-                    <img src={checkLogo} className="left-logo" alt="check-icon" />
-                    <a href='www.baidu.com'>教练审核</a>
-                </li>
-                <li>
-                    <img src={manageLogo} className="left-logo" alt="manage-icon" />
-                    <a href='www.baidu.com'>教练管理</a>
-                </li>
-                <li>
-                <img src={unlockLogo} className="left-logo" alt="unlock-icon" />
-                <a href='www.baidu.com'>教学管理</a>
-                </li>
-            </ul>
+        <div className="HeadSide">
+            <img src={avatar} alt="admin" className="headAdmin"/>
+
+            <div className="adminInfo">
+                <img src={userLogo} alt="manage" className="adminLogo"/>
+                <text className="adminName">admin-username</text>
+            </div>
+
+            <div className="sideItem" id="check" style={firstStyle} onClick={jumpCheck}>
+                <img src={checkLogo} alt="check-icon" className="sidePic"/>
+                <text className="sideText">教练审核</text>
+            </div>
+
+            <div className="sideItem" id="manage" style={otherStyle} onClick={jumpManage}>
+                <img src={manageLogo} alt="manage-icon" className="sidePic"/>
+                <text className="sideText" style={otherTextStyle}>教练管理</text>
+            </div>
+
+            <div className="sideItem" id="unlock" style={otherStyle} onClick={jumpUnlock}>
+                <img src={unlockLogo} alt="unlock-icon" className="sidePic"/>
+                <text className="sideText" style={otherTextStyle}>教学管理</text>
+            </div>
 
         </div>
     )
 }
 
 export default Left;
+
+function jumpCheck() {
+    document.getElementById("check").style.backgroundColor = '#F2C94C';
+    document.getElementById("manage").style.backgroundColor = 'white';
+    document.getElementById("unlock").style.backgroundColor = 'white';
+
+    let text = document.getElementsByClassName("sideText");
+    text[0].style.fontWeight = 'bold';
+    text[1].style.fontWeight = 'normal';
+    text[2].style.fontWeight = 'normal';
+}
+
+function jumpManage() {
+    document.getElementById("manage").style.backgroundColor = '#F2C94C';
+    document.getElementById("check").style.backgroundColor = 'white';
+    document.getElementById("unlock").style.backgroundColor = 'white';
+
+    let text = document.getElementsByClassName("sideText");
+    text[1].style.fontWeight = 'bold';
+    text[0].style.fontWeight = 'normal';
+    text[2].style.fontWeight = 'normal';
+}
+
+function jumpUnlock() {
+    document.getElementById("unlock").style.backgroundColor = '#F2C94C';
+    document.getElementById("manage").style.backgroundColor = 'white';
+    document.getElementById("check").style.backgroundColor = 'white';
+
+    let text = document.getElementsByClassName("sideText");
+    text[2].style.fontWeight = 'bold';
+    text[1].style.fontWeight = 'normal';
+    text[0].style.fontWeight = 'normal';
+}
